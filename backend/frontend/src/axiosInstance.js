fddf897bd1a901dcbb4aae070b9dae21d4fe24f2
SreeshwanMaralakshikari <24eg105q47@anurag.edu.in>
2026-08-29 01:17:05 +0530

    commit for improve 3

--- backend/frontend/src/axiosInstance.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/notice-api";
-const axiosInstance = axios.create({ baseURL });
-
-export default axiosInstance;
